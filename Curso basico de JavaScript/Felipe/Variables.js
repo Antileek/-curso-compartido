7f33850ef7ecd3bf0cace2d610dd01a0051898bf
Felipe Salas <felipe_jesus724@hotmail.com>
2021-02-16 23:01:44 -0700

    JavaScript Funciones, valores y variables

---
+++ Curso basico de JavaScript/Felipe/Variables.js
@@ -0,0 +1,18 @@
+var nombre = "Felipe";
+
+// declarar
+var edad;
+// inicializar 
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombr: "Felipe",
+    edad: 30
+}
+
+persona;
+
